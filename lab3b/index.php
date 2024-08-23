30931efefe8f176a8b7f4764ab73c48adbc6392f
Aetrix <markjeromesantos102@gmail.com>
2024-08-23 15:32:11 +0800

    fix form

--- lab3b/index.php
+++ lab3b/index.php
@@ -1,46 +1,49 @@
 <html>
+
 <head>
-    <meta charset="utf-8">
-    <title>IPT10 Laboratory Activity #2</title>
-    <link rel="icon" href="https://phpsandbox.io/assets/img/brand/phpsandbox.png">
-    <link rel="stylesheet" href="https://assets.ubuntu.com/v1/vanilla-framework-version-4.15.0.min.css" />   
+  <meta charset="utf-8">
+  <title>IPT10 Laboratory Activity #2</title>
+  <link rel="icon" href="https://phpsandbox.io/assets/img/brand/phpsandbox.png">
+  <link rel="stylesheet" href="https://assets.ubuntu.com/v1/vanilla-framework-version-4.15.0.min.css" />
 </head>
 
 <body style="background-color: pink;">
-<div class="u-fixed-width">
-  <div class="p-logo-section">
-    <div class="p-logo-section__items">
-      <div class="p-logo-section__item">
-        <img class="p-logo-section__logo" src="https://www.auf.edu.ph/home/images/logo2.png" alt="Angeles University Foundation">
+  <div class="u-fixed-width">
+    <div class="p-logo-section">
+      <div class="p-logo-section__items">
+        <div class="p-logo-section__item">
+          <img class="p-logo-section__logo" src="https://www.auf.edu.ph/home/images/logo2.png"
+            alt="Angeles University Foundation">
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<div class="row--50-50 grid-demo">
-  <div class="col">
-    <h4>File Upload</h4>
+  <div class="row--50-50 grid-demo">
+    <div class="col">
+      <h4>File Upload</h4>
 
-    <form>
+      <form method="POST" action="uploaded.php" enctype="multipart/form-data">
         <div class="p-card">
-            <h3>Text File</h3>
-            <p class="p-card__content">
+          <h3>Text File</h3>
+          <p class="p-card__content">
             <input type="file" name="text_file" accept=".txt" />
-            </p>
+          </p>
         </div>
 
         <div>
-            <button>
-                Upload
-            </button>
+          <button type="submit">
+            Upload
+          </button>
         </div>
-    </form>
+      </form>
     </div>
-  <div class="col">
-  <img class="p-logo-section__logo" src="https://www.auf.edu.ph/home/images/mascot/CCS.png" alt="College of Computing Studies">
+    <div class="col">
+      <img class="p-logo-section__logo" src="https://www.auf.edu.ph/home/images/mascot/CCS.png"
+        alt="College of Computing Studies">
+    </div>
   </div>
-</div>
 
 </body>
-</html>
 
+</html>
